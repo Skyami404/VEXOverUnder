833a4288d47368e27342be09d3ee4c1aa4deb6ca
Skyami <jaanvi0403@gmail.com>
2023-09-30 17:17:31 -0700

    renamed

---
+++ 2tri_touch/include/robot-config.h
@@ -0,0 +1,31 @@
+// DO NOT CHANGE THIS FILE
+
+using namespace vex;
+using signature = vision::signature;
+extern brain Brain;
+extern controller Controller;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern motor_group LeftDriveSmart;
+extern motor_group RightDriveSmart;
+extern motor cata;
+extern motor intake;
+extern motor arm;
+
+extern inertial Inertia;
+extern rotation Rotation;
+
+extern digital_out wing1;
+extern digital_out wing2;
+
+extern timer PidDriveTimer;
+extern timer Debounce;
+
+
+/** 
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
